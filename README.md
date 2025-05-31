26f9992e496ccd7cc5d845925e0f2c0cd3044760
shenleban tongying <shenlebantongying@gmail.com>
2025-05-30 22:07:46 -0400

    disk persistence

--- README.md
+++ README.md
@@ -1,18 +1,35 @@
-# Dead Simple, Efficient, Absolutely No Distraction StopWatch for Linux
-
-A particular type of stopwatch.
+## StopWatch, Dead Simple, Efficient, Absolutely No Distraction
 
 Similar to
 
 - macOS -> [MenuStopWatch](https://github.com/shenlebantongying/MenuStopWatch_macOS) by me.
 - Android -> [Tranquil Stopwatch](https://github.com/tibarj/tranquilstopwatch/)
 
-# Spotlight
+It has 2 operations:
 
-The time is stored to disk, and the app doesn't need to be running in the background.
+- Click -> Pause/Unpause
+- Long Press -> Restart
 
-# Known Flaw
+The recording is stored to disk. The app doesn't need to be running in the background, and there no worries if the app is accidentablly closed.
 
-Internally, the Unix epoch time is used. When leap seconds happen, the stopwatch will be off by 1. However, leap seconds only happened 27 times as of 2025.
+
+<img src="https://github.com/user-attachments/assets/10c8a03d-98d4-4d0d-8727-07c6af44eb2f" height=200>
+<img src="https://github.com/user-attachments/assets/77c3c171-9b76-4547-b58f-e9e65409c111" height=200>
+
+## Support
+
+Feature requests will be immediately rejected. Any addition to this program is distracting bloat.
+
+
+## Known Flaw
+
+Internally, the Unix epoch time is used. It causes two problems.
+
+1. When leap seconds happen, the stopwatch will be off by 1. However, leap seconds only happened 27 times as of 2025.
+2. Pausing and restarting will cause inaccuracy for a maximum of 1 second.
 
 https://man7.org/linux/man-pages/man2/time.2.html
+
+## LICENSE
+
+[GPL-3.0-or-later](https://spdx.org/licenses/GPL-3.0-or-later.html)
--- mStopWatch.cpp
+++ mStopWatch.cpp
@@ -3,179 +3,242 @@
 #include <ctime>
 #include <format>
 
-namespace
-{
+namespace {
 using i64 = int64_t;
 
-struct UI
-{
-  GtkLabel_autoptr label = nullptr;
-  GtkWindow_autoptr window = nullptr;
-  AdwToolbarView_autoptr toolbar_view = nullptr;
-  GtkWidget_autoptr headbar = nullptr;
+struct UI {
+    GtkLabel_autoptr label = nullptr;
+    GtkWindow_autoptr window = nullptr;
+    AdwToolbarView_autoptr toolbar_view = nullptr;
+    AdwHeaderBar_autoptr headbar = nullptr;
 
-  GtkGesture_autoptr ges_shortclick = nullptr;
-  GtkGesture_autoptr ges_longpress = nullptr;
+    GtkGesture_autoptr ges_shortclick = nullptr;
+    GtkGesture_autoptr ges_longpress = nullptr;
 };
 
-struct States
-{
-  time_t current_timestamp = 0;
-  i64 pause_duration = 0;
-  bool paused = false;
+constexpr int state_file_version = 1;
+
+struct States {
+    time_t current_timestamp = 0;
+    i64 pause_duration = 0;
+    bool paused = false;
 };
 
-UI ui{};
-States states{};
+UI ui {};
+States states {};
 guint gsource_id = 0;
 
-PangoAttrList *font_attrs_default;
-PangoAttrList *font_attrs_red;
+PangoAttrList* font_attrs_default;
+PangoAttrList* font_attrs_red;
 
 ///
 
 time_t
-get_current_time ()
+get_current_time()
 {
-  time_t t = time (nullptr);
-  if (t == ((time_t) -1))
-    {
-      throw std::runtime_error ("Failed to get unix epoch");
+    time_t t = time(nullptr);
+    if (t == ((time_t)-1)) {
+        throw std::runtime_error("Failed to get unix epoch");
     }
 
-  return t;
+    return t;
 }
 
 std::string
-GetTimeString (i64 v)
+GetTimeString(i64 v)
 {
-  constexpr i64 seconds_in_hour = 60 * 60;
-  constexpr i64 seconds_in_min = 60;
+    constexpr i64 seconds_in_hour = 60 * 60;
+    constexpr i64 seconds_in_min = 60;
 
-  auto hour_div = std::div (v, seconds_in_hour);
-  i64 n_hours = hour_div.quot;
+    auto hour_div = std::div(v, seconds_in_hour);
+    i64 n_hours = hour_div.quot;
 
-  auto minutes_div = std::div (hour_div.rem, seconds_in_min);
-  i64 n_minutes = minutes_div.quot;
-  i64 n_seconds = minutes_div.rem;
+    auto minutes_div = std::div(hour_div.rem, seconds_in_min);
+    i64 n_minutes = minutes_div.quot;
+    i64 n_seconds = minutes_div.rem;
 
-  return std::format ("{}:{:02}:{:02}", n_hours, n_minutes, n_seconds);
+    return std::format("{}:{:02}:{:02}", n_hours, n_minutes, n_seconds);
 }
 
-gboolean
-inc_paused_duration ()
+///
+
+std::string states_to_str()
 {
-  states.pause_duration += 1;
-  return TRUE;
+    return std::format("{}\n{}\n{}\n{}\n", state_file_version, states.current_timestamp, states.pause_duration, states.paused);
 }
 
-gboolean
-ticking ()
+void states_write()
 {
-  auto s = GetTimeString (get_current_time () - states.current_timestamp - states.pause_duration);
-  gtk_label_set_text (ui.label, s.c_str ());
-  return TRUE;
-}
-
-gboolean
-start_reset ()
-{
-
-  gtk_label_set_attributes (ui.label, font_attrs_default);
-  if (gsource_id > 0)
-    {
-      g_source_destroy (g_main_context_find_source_by_id (g_main_context_default (), gsource_id));
+    g_autofree GError* err = nullptr;
+    g_file_set_contents("./wtf.txt", states_to_str().c_str(), states_to_str().length(), &err);
+    if (err != nullptr) {
+        throw std::runtime_error(std::format("Cannot write file {}", err->message));
     }
-  states.current_timestamp = get_current_time ();
-  states.pause_duration = 0;
-
-  ticking ();
-  gsource_id = g_timeout_add_seconds (1, GSourceFunc (ticking), nullptr);
-  return TRUE;
 }
 
-gboolean
-pause_unpause ()
+bool states_read()
 {
-  g_source_destroy (g_main_context_find_source_by_id (g_main_context_default (), gsource_id));
-  if (states.paused)
-    {
-      gtk_label_set_attributes (ui.label, font_attrs_default);
-      states.paused = false;
-
-      ticking ();
-      gsource_id = g_timeout_add_seconds (1, GSourceFunc (ticking), nullptr);
-    }
-  else
-    {
-      gtk_label_set_attributes (ui.label, font_attrs_red);
-      states.paused = true;
-
-      inc_paused_duration ();
-      gsource_id = g_timeout_add_seconds (1, GSourceFunc (inc_paused_duration), nullptr);
+    g_autofree GError* err = nullptr;
+    g_autofree gchar* content = nullptr;
+    gsize length = 0;
+    g_file_get_contents("./wtf.txt", &content, &length, &err);
+    if (err != nullptr) {
+        throw std::runtime_error(std::format("Cannot read file {}", err->message));
     }
 
-  return TRUE;
+    gchar** string_segs = g_strsplit(content, "\n", 0);
+
+    if (g_strv_length(string_segs) != 5 || g_ascii_strtoll(string_segs[0], NULL, 10) != state_file_version) {
+        g_strfreev(string_segs);
+        return false;
+    } else {
+        states.current_timestamp = g_ascii_strtoll(string_segs[1], NULL, 10);
+        states.pause_duration = g_ascii_strtoll(string_segs[2], NULL, 10);
+        states.paused = (string_segs[3] == "true");
+
+        g_strfreev(string_segs);
+        return true;
+    }
+}
+
+///
+
+gboolean
+inc_paused_duration()
+{
+    states.pause_duration += 1;
+    return TRUE;
+}
+
+gboolean
+ticking()
+{
+    auto s = GetTimeString(get_current_time() - states.current_timestamp - states.pause_duration);
+    gtk_label_set_text(ui.label, s.c_str());
+    return TRUE;
+}
+
+gboolean
+start()
+{
+    gtk_label_set_attributes(ui.label, font_attrs_default);
+    if (gsource_id > 0) {
+        g_source_destroy(g_main_context_find_source_by_id(g_main_context_default(), gsource_id));
+    }
+
+    if (!states_read()) {
+        states.current_timestamp = get_current_time();
+        states.pause_duration = 0;
+        states.paused = false;
+    }
+
+    ticking();
+    gsource_id = g_timeout_add_seconds(1, GSourceFunc(ticking), nullptr);
+    states_write();
+    return TRUE;
+}
+
+gboolean
+reset()
+{
+    gtk_label_set_attributes(ui.label, font_attrs_default);
+    if (gsource_id > 0) {
+        g_source_destroy(g_main_context_find_source_by_id(g_main_context_default(), gsource_id));
+    }
+
+    states.current_timestamp = get_current_time();
+    states.pause_duration = 0;
+    states.paused = false;
+
+    ticking();
+    gsource_id = g_timeout_add_seconds(1, GSourceFunc(ticking), nullptr);
+    states_write();
+    return TRUE;
+}
+
+gboolean
+pause_unpause()
+{
+    g_source_destroy(g_main_context_find_source_by_id(g_main_context_default(), gsource_id));
+    if (states.paused) {
+        gtk_label_set_attributes(ui.label, font_attrs_default);
+        states.paused = false;
+
+        ticking();
+        gsource_id = g_timeout_add_seconds(1, GSourceFunc(ticking), nullptr);
+    } else {
+        gtk_label_set_attributes(ui.label, font_attrs_red);
+        states.paused = true;
+
+        inc_paused_duration();
+        gsource_id = g_timeout_add_seconds(1, GSourceFunc(inc_paused_duration), nullptr);
+    }
+
+    states_write();
+
+    return TRUE;
 }
 
 // GUI
 
-void
-init (GtkApplication *app)
-{
-  ui.window = (GtkWindow *) adw_application_window_new (app);
-  gtk_window_set_resizable (ui.window, FALSE);
-  gtk_window_set_default_size (ui.window, 200, 0);
-
-  ui.label = (GtkLabel *) gtk_label_new ("-:--:--");
-
-  ui.ges_shortclick = gtk_gesture_click_new ();
-  gtk_widget_add_controller (GTK_WIDGET (ui.label), GTK_EVENT_CONTROLLER (ui.ges_shortclick));
-  g_signal_connect (ui.ges_shortclick, "pressed", G_CALLBACK (pause_unpause), NULL);
-
-  ui.ges_longpress = gtk_gesture_long_press_new ();
-  gtk_widget_add_controller (GTK_WIDGET (ui.label), GTK_EVENT_CONTROLLER (ui.ges_longpress));
-  g_signal_connect (ui.ges_longpress, "pressed", G_CALLBACK (start_reset), NULL);
-
-  ui.toolbar_view = (AdwToolbarView *) adw_toolbar_view_new ();
-  ui.headbar = adw_header_bar_new ();
-
-  adw_toolbar_view_add_top_bar (ADW_TOOLBAR_VIEW (ui.toolbar_view), ui.headbar);
-
-  adw_toolbar_view_set_content (ADW_TOOLBAR_VIEW (ui.toolbar_view), GTK_WIDGET (ui.label));
-
-  adw_application_window_set_content (ADW_APPLICATION_WINDOW (ui.window), GTK_WIDGET (ui.toolbar_view));
-
-  start_reset ();
-  gtk_window_present (GTK_WINDOW (ui.window));
-}
-
-void
-init_font_attrs ()
+void cb_startup(GtkApplication* app)
 {
 
-  PangoFontDescription *fond_desc = pango_font_description_new ();
-  pango_font_description_set_size (fond_desc, 36 * PANGO_SCALE);
+    ui.window = (GtkWindow*)adw_application_window_new(app);
+    gtk_window_set_resizable(ui.window, FALSE);
 
-  font_attrs_default = pango_attr_list_new ();
-  pango_attr_list_insert (font_attrs_default, pango_attr_font_desc_new (fond_desc));
+    ui.label = (GtkLabel*)gtk_label_new("-:--:--");
 
-  font_attrs_red = pango_attr_list_new ();
+    ui.ges_shortclick = gtk_gesture_click_new();
+    gtk_widget_add_controller(GTK_WIDGET(ui.label), GTK_EVENT_CONTROLLER(ui.ges_shortclick));
+    g_signal_connect(ui.ges_shortclick, "pressed", G_CALLBACK(pause_unpause), NULL);
 
-  pango_attr_list_insert (font_attrs_red, pango_attr_font_desc_new (fond_desc));
-  pango_attr_list_insert (font_attrs_red, pango_attr_foreground_new (65535, 0, 0));
+    ui.ges_longpress = gtk_gesture_long_press_new();
+    gtk_widget_add_controller(GTK_WIDGET(ui.label), GTK_EVENT_CONTROLLER(ui.ges_longpress));
+    g_signal_connect(ui.ges_longpress, "pressed", G_CALLBACK(reset), NULL);
+
+    ui.toolbar_view = (AdwToolbarView*)adw_toolbar_view_new();
+    ui.headbar = ADW_HEADER_BAR(adw_header_bar_new());
+
+    adw_toolbar_view_add_top_bar(ADW_TOOLBAR_VIEW(ui.toolbar_view), GTK_WIDGET(ui.headbar));
+
+    adw_toolbar_view_set_content(ADW_TOOLBAR_VIEW(ui.toolbar_view), GTK_WIDGET(ui.label));
+
+    adw_application_window_set_content(ADW_APPLICATION_WINDOW(ui.window), GTK_WIDGET(ui.toolbar_view));
+
+    start();
+    gtk_window_present(GTK_WINDOW(ui.window));
 }
 
-}
-
-int
-main (int argc, char *argv[])
+void cb_activate()
 {
-  init_font_attrs ();
+    gtk_window_present(GTK_WINDOW(ui.window));
+}
 
-  g_autoptr (AdwApplication) app = adw_application_new ("org.slbtty.mStopWatch", G_APPLICATION_DEFAULT_FLAGS);
+void init_font_attrs()
+{
 
-  g_signal_connect (app, "activate", G_CALLBACK (init), NULL);
+    PangoFontDescription* fond_desc = pango_font_description_new();
+    pango_font_description_set_size(fond_desc, 36 * PANGO_SCALE);
 
-  return g_application_run (G_APPLICATION (app), argc, argv);
+    font_attrs_default = pango_attr_list_new();
+    pango_attr_list_insert(font_attrs_default, pango_attr_font_desc_new(fond_desc));
+
+    font_attrs_red = pango_attr_list_new();
+
+    pango_attr_list_insert(font_attrs_red, pango_attr_font_desc_new(fond_desc));
+    pango_attr_list_insert(font_attrs_red, pango_attr_foreground_new(65535, 0, 0));
+}
+}
+
+int main(int argc, char* argv[])
+{
+    init_font_attrs();
+
+    g_autoptr(AdwApplication) app = adw_application_new("org.slbtty.mstopwatch", G_APPLICATION_DEFAULT_FLAGS);
+
+    g_signal_connect(app, "startup", G_CALLBACK(cb_startup), NULL);
+    g_signal_connect(app, "activate", G_CALLBACK(cb_activate), NULL);
+
+    return g_application_run(G_APPLICATION(app), argc, argv);
 }
